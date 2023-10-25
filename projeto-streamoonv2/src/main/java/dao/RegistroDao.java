1430fa12e71a61f8d698b70404a331c8746a3576
Lucas Neves <125586223+lucasrocha2704@users.noreply.github.com>
2023-10-25 13:50:26 -0300

    correção data

--- projeto-streamoonv2/src/main/java/dao/RegistroDao.java
+++ projeto-streamoonv2/src/main/java/dao/RegistroDao.java
@@ -4,6 +4,9 @@ import models.RegistroModel;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -21,15 +24,19 @@ public class RegistroDao {
 
     public RegistroDao(){
         this.con = conexao.getConexaoDoBanco();
-        this.horario = LocalDateTime.now();
         this.local = new Locale("pt","BR");
-        this.formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss",local);
         this.componenteDao = new ComponenteDao();
     }
 
     public Integer inserirDadosBanco(Double registro, Integer fkComponente) {
+        
+        this.horario = LocalDateTime.now();
+        this.formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss",local);
+
+        BigDecimal reg = new BigDecimal(registro).setScale(1, RoundingMode.HALF_EVEN);
+
         String insert = "INSERT INTO registro (registro, dtHora, fkComponenteServidor) VALUES (?, ?, ?)";
-        return con.update(insert, registro, formatter.format(horario), fkComponente);
+        return con.update(insert, reg.doubleValue(), formatter.format(horario), fkComponente);
     }
 
     public List<RegistroModel> selectComponente(String componente) {
